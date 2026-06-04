4b10699fe332146318d1bdb73dea74ec2c3a9da3
SIIDigitalCreative <design@sunbeamsimpexinc.com>
2026-06-04 08:49:31 +0800

    Update redis.ts

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,9 @@
 import { Redis } from '@upstash/redis'
 
-export const redis = Redis.fromEnv()
+export const redis = new Redis({
+  url: process.env.KV_REST_API_URL || process.env.UPSTASH_REDIS_REST_URL || '',
+  token: process.env.KV_REST_API_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN || '',
+})
 
 export const KEYS = {
   products: 'quencha:products',
